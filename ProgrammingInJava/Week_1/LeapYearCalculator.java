4b9eb3b28645665fa3e00ad05e074a01c6c54ccd
felixportuphy <77155009+felixportuphy@users.noreply.github.com>
2021-01-17 18:17:35 +0000

    Update LeapYearCalculator.java

--- ProgrammingInJava/Week_1/LeapYearCalculator.java
+++ ProgrammingInJava/Week_1/LeapYearCalculator.java
@@ -4,11 +4,22 @@ public class LeapYearCalculator {
     public static boolean isLeap(int year) {
 
         if(year>=1 && year<=9999){
-            if( ( (year % 4==0) && (year % 100!=0) )|| (year % 400==0) ) {
-                return true;
-            }
-            else
-                return false;
+           if(year % 4== 0){
+              if(year % 100== 0){
+                  if(year % 400== 0){
+                      return true;
+                  }
+                  else {
+                      return false;
+                  }
+              }
+              else {
+                  return true;
+              }
+           }
+           else {
+               return false;
+           }
         }
         else
             return false;
